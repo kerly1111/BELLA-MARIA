25de8cb0c9c87efecc2a2ac0e8b4e3036a1e0776
cristhian1111 <cristhianj.armijosc@gmail.com>
2016-02-27 20:25:27 -0500

    aa

---
+++ Admin/usuario.php
@@ -0,0 +1,47 @@
+<?php include 'stilosFunciones.php' ?>
+<?php include 'cabecera.php' ?>
+
+<form role="form" action="" name="frmClientes">
+              
+              <div class="col-lg-4 col-md-offset-4
+              ">
+                <div class="form-group"><br>
+                  <label>Cedula</label>
+                  <input name="cedula" class="form-control" required>
+                </div>
+
+                <div class="form-group">
+                  <label>Nombre</label>
+                  <input name="nombre" class="form-control" required>
+                </div>
+
+                <div class="form-group">
+                  <label>Apellido</label>
+                  <input name="apellido" class="form-control" required>
+                </div>
+
+                <div class="form-group">
+                  <label>Direccion</label>
+                  <input name="direccion" class="form-control" required>
+                </div>
+
+                <div class="form-group">
+                  <label>Telefono</label>
+                  <input name="telefono" class="form-control" required>
+                </div>
+
+                <div class="form-group">
+                  <label>Correo</label>
+                  <input name="correo" class="form-control" required>
+                </div>
+
+                <div class="form-group">
+                  <label>Tipo Cliente</label>
+                  <input name="tipocliente" class="form-control" required>
+                </div>
+
+             
+              </div>
+              
+            </form>
+
