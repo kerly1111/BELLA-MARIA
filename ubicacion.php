af7242275dfdd3a460f20761318174d6c68bb9e7
kerly1111 <krmc_94@hotmail.com>
2016-02-28 19:17:38 -0500

    c

--- ubicacion.php
+++ ubicacion.php
@@ -1,67 +1,41 @@
-<html>
-<head>
-<title>Local</title>
-<script src="http://j.maxmind.com/app/geoip.js"></script>
-<script src="http://maps.google.com/maps/api/js?sensor=false"></script>
-<script type="text/javascript" >
+<?php include 'cabecera.php' ?>
+<section id="contact-page" class="container">
+      <div align="center">
 
-function getGeo(){
+      <div >
+      <button class="btn btn-default" onclick="mapa();" type="button"><span class="glyphicon glyphicon-globe"></span>&nbsp;&nbsp; Ubicación</button>
+      &nbsp;&nbsp;&nbsp;&nbsp;
+      <button class="btn btn-default" onclick="comoLlegar();" type="button"><span class="glyphicon glyphicon-map-marker"></span>&nbsp;&nbsp; Como LLegar</button>
+      </div>
+      <hr>
+      <div id="map" class="dimencionMApa" ></div>
+      </div>
 
-if (navigator && navigator.geolocation) {
-navigator.geolocation.getCurrentPosition(geoOK, geoKO);
-} else {
-geoMaxmind();
-}
 
-}
+          <script type="text/javascript">
+            mapa();
+            
+            function mapa(){
+              var map;
+              $(document).ready(function(){
+              map = new GMaps({
+              div: '#map',
+              lat:-3.762020,
+              lng:-79.847104,
+              height:'500px',
+              width:'800px'       
+            });            
+              });
+            }
+            function comoLlegar(){  
+               
 
-function geoOK(position) {
-showLatLong(position.coords.latitude, position.coords.longitude);
-}
-function geoMaxmind() {
-showLatLong(geoip_latitude(), geoip_longitude());
-}
+              }          
 
-function geoKO(err) {
-if (err.code == 1) {
-error('El usuario ha denegado el permiso para obtener informacion de ubicacion.');
-} else if (err.code == 2) {
-error('Tu ubicacion no se puede determinar.');
-} else if (err.code == 3) {
-error('TimeOut.')
-} else {
-error('No sabemos que pasó pero ocurrio un error.');
-}
-}
 
-function showLatLong(lat, longi) {
-var geocoder = new google.maps.Geocoder();
-var yourLocation = new google.maps.LatLng(lat, longi);
-geocoder.geocode({ 'latLng': yourLocation },processGeocoder);
+            </script>
 
-}
-function processGeocoder(results, status){
 
-if (status == google.maps.GeocoderStatus.OK) {
-if (results[0]) {
-document.forms[0].dir.value=results[0].formatted_address;
-} else {
-error('Google no retorno resultado alguno.');
-}
-} else {
-error("Geocoding fallo debido a : " + status);
-}
-}
-function error(msg) {
-alert(msg);
-}
-</script>
-</head>
-<body>
-<h1>Informaci&oacute;n de Geolocalizaci&oacute;n</h1>
-<form>
-<input type="button" onclick="getGeo()" value="Obtener Latitud y Longitud"/>
-<input type="text" name="dir" width="150"/>
-</form>
-</body>
-</html>
+    </section><!--/#contact-page-->
+
+<?php include 'pie.php' ?>
