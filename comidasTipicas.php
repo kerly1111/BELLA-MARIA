6711611bf746d17b3f1a536093f31fcfb68959dc
cristhian1111 <cristhianj.armijosc@gmail.com>
2016-03-07 03:59:56 -0500

    comidastipicas

---
+++ comidasTipicas.php
@@ -0,0 +1,99 @@
+<?php include 'cabecera.php' ?>
+<?php include 'panelNoticias.php' ?>
+
+<div class="panel panel-success">
+  <div class="panel-heading"><h4>COMIDADS TÍPICAS</h4></div>
+  <div class="panel-body">
+    <p style="text-align: justify;">
+     		La Parroquia bellamaria promueve al turista con deliciosos platos típicos de entre ellos hemos destacado el Parihuela, el cual es una exquisita sopa marinera acompañada de patacones. Es mas conocida por sus propiedades afrodisíacas. Además podrá disfrutar del rico cebiche, el cual le ofrecen en muchas variedades como los de camarón, concha, pescado, mixtos, la mayoría disfruta de platos hechos a base del producto principal de la zona (platano) y otros mariscos. Así que no pueden dejar de saborearlos.
+
+     		<div class="row">
+
+				  <div class="col-md-4">
+				  <div class="panel panel-success">
+  					<div class="panel-heading"><h3>PARIHUELA</h3></div>
+					  <div class="panel-body">
+					    <div align="center">
+							<IMG  SRC="imagenes/064.png"  WIDTH=550 HEIGHT=300 BORDER=3 class="img-responsive" alt="Imagen responsive">
+							<br>
+
+		
+		               </div>
+					  </div>
+				  </div>
+
+				  <div class="panel panel-success">
+  					<div class="panel-heading"><h3>CALDO Y SECO DE GALLINA</h3></div>
+					  <div class="panel-body">
+					    <div align="center">
+							<IMG  SRC="imagenes/014.png"  WIDTH=550 HEIGHT=300 BORDER=3 class="img-responsive" alt="Imagen responsive">
+							<br>
+
+		
+		               </div>
+					  </div>
+				  </div>
+
+				  </div>
+				  <div class="col-md-4">
+
+				  	<div class="panel panel-success">
+  					<div class="panel-heading"><h3>PATACONES CON HUEVO</h3></div>
+					  <div class="panel-body">
+					    	<div align="center">
+							<IMG  SRC="imagenes/011.png"  WIDTH=550 HEIGHT=300 BORDER=3 class="img-responsive" alt="Imagen responsive">
+							<br>
+
+							
+							</div>
+					  </div>
+				  </div>
+
+				  <div class="panel panel-success">
+  					<div class="panel-heading"><h3>PATACONES CON QUESO</h3></div>
+					  <div class="panel-body">
+					    <div align="center">
+							<IMG  SRC="imagenes/013.png"  WIDTH=550 HEIGHT=300 BORDER=3 class="img-responsive" alt="Imagen responsive">
+							<br>
+
+		
+		               </div>
+					  </div>
+				  </div>
+
+				  </div>
+				  <div class="col-md-4">
+
+				  <div class="panel panel-success">
+  					<div class="panel-heading"><h3>FRITADA</h3></div>
+					  <div class="panel-body">
+					   		<div align="center">
+								<IMG  SRC="imagenes/010.png"  WIDTH=550 HEIGHT=300 BORDER=3 class="img-responsive" alt="Imagen responsive">
+								<br>
+
+								
+							</div>
+					  </div>
+				  </div>
+
+				  <div class="panel panel-success">
+  					<div class="panel-heading"><h3>BOLONES DE VERDE</h3></div>
+					  <div class="panel-body">
+					    <div align="center">
+							<IMG  SRC="imagenes/012.png"  WIDTH=550 HEIGHT=300 BORDER=3 class="img-responsive" alt="Imagen responsive">
+							<br>
+
+		
+		               </div>
+					  </div>
+				  </div>
+
+				  </div>
+			
+			</div>
+
+
+     		<br>     		
+    	</p>
+  </div>
+</div>
