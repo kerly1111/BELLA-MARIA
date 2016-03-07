3ed35bda465f27e4555c83301290181072333afd
cristhian1111 <cristhianj.armijosc@gmail.com>
2016-03-07 02:10:33 -0500

    hola mi amor te amo

---
+++ gad.php
@@ -0,0 +1,68 @@
+<?php include 'cabecera.php' ?>
+<?php include 'panelNoticias.php' ?>
+
+<div class="panel panel-success">
+  <div class="panel-heading"><h3>Mensaje del Presidente</h3></div>
+  <div class="panel-body">
+    
+  <div class="row">
+	  <div class="col-md-4"></div>
+	  <div class="col-md-4 col-md-offset-1">
+   	<IMG SRC="imagenes/089.png" WIDTH=250 HEIGHT=300 BORDER=3>
+
+	  </div>
+	  <div class="col-md-4"></div>
+  </div>
+
+  <p style="text-align: justify;">
+  	Ante la necesidad urgente de atender la problemática que atraviesa nuestra parroquia en cuanto a su desarrollo local, hemos decidido afrontar un reto  que es la formulación del plan de desarrollo estratégico parroquial de Bellamaría, proceso que ha sido una tarea dinámica, participativa y de compromiso, porque han confluido factores de viabilidad política (gestión del gobierno parroquial), participación ciudadana, cooperación externa (coordinación técnica provincial) y concertación de propuestas de desarrollo entre los actores participantes.<br><br>
+
+Pensado en los nuevos desafíos y oportunidades de nuestro territorio y de nuestra gente, se ha logrado formular y  disponer de una herramienta de gestión del desarrollo local. Guía que va orientar la  inversión y mejor distribución de los recursos del estado, con una visión estratégica integral, que vaya más allá de la infraestructura u obras de corto plazo, sino también hacia la inversión productiva, social, protección y conservación de los recursos naturales.<br><br>
+
+El Gobierno Autónomo Descentralizado Parroquial  de Bellamaría Presenta  ante  la comunidad, local, nacional y internacional, la página web institucional www.bellamariasantarosa.gob.ec  medio por el cual la ciudadanía y entes de control podrán tener acceso a la información pública, referente a la gestión y manejo de los fondos públicos.<br><br>
+
+Esta administración, será referencia del desarrollo ético y moral, por el  trabajo y lucha constante en busca de los sueños de nuestros moradores, sobre la base del plan de desarrollo y ordenamiento territorial y de los ejes del progreso de los pueblos como son: educación, salud, vialidad, seguridad, cultura, deporte, recreación, turismo, medio ambiente, vivienda; y, producción, teniendo como resultado un Gobierno  Parroquial, comprometido con los más necesitados;  creando oportunidades que mejoren la calidad de vida de nuestros  habitantes de los diferentes recintos y barrios.<br><br>
+</p>
+<p style="text-align: center;">
+ VIVIR NO ES SOLO EXITIR Y CREAR, SABER GOZAR Y SUFRIR Y NO DORMIR SIN SOÑAR, DESCANSAR, ES EMPEZAR A MORIR.<br>
+
+GREGORIO MARAÑON….<br>
+
+Sr. Jaime Durán Chuchuca<br>
+
+Presidente del Gobierno parroquial de Bellamaría 2009-2014
+ </p>
+
+<div class="row">
+  <div class="col-md-4">video 1
+
+  <div class="panel panel-default">
+  <div class="panel-heading">Título del panel con estilo normal</div>
+  <div class="panel-body">
+    Contenido del panel
+  </div>
+	</div>
+
+  </div>
+  <div class="col-md-4">video 2
+
+<div class="panel panel-default">
+  <div class="panel-heading">Título del panel con estilo normal</div>
+  <div class="panel-body">
+    Contenido del panel
+  </div>
+</div>
+
+  </div>
+  <div class="col-md-4">video 3
+<div class="panel panel-default">
+  <div class="panel-heading">Título del panel con estilo normal</div>
+  <div class="panel-body">
+    Contenido del panel
+  </div>
+</div>
+  </div>
+</div>
+  </div>
+
+</div>
