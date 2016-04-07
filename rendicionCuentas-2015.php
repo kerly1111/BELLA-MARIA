5535737fbfb7d302aec590a321e10fa67da94370
cristhian1111 <cristhianj.armijosc@gmail.com>
2016-04-07 12:54:30 -0500

    rendcion de cuenstas

---
+++ rendicionCuentas-2015.php
@@ -0,0 +1,36 @@
+<?php include 'cabecera.php' ?>
+<?php include 'banner.php' ?>
+
+
+<div class="panel panel-success">
+  <div class="panel-heading"><h3>Rendición de Cuentas 2015</h3></div>
+  <div class="panel-body">
+    <p style="text-align: justify;">
+
+    <u><a href="Archivos/rendicionCuentas2015/1CONSTITUCION_DE_LA_REPUBLICA_DEL_ECUADOR.pdf" target="_blank">CONVOCATORiA RENDICIóN DE CUENTAS AÑO 2015.</a></u><br>
+      <u><a href="Archivos/rendicionCuentas2015/BELLA_SANTA_RENDICION_DE_CUENTAS_2015-2016.pdf" target="_blank">RENDICIóN DE CUENTAS 2015 - 2016.</a></u><br>
+      <u><a href="Archivos/rendicionCuentas2015/REGISTRO_DE_FIRMAS_RENDICION_DE_CUENTAS_2015.pdf" target="_blank"> REGISTRO DE FIRMAS RENDICIóN DE CUENTAS 2015.</a></u><br>
+      <u><a href="Archivos/rendicionCuentas2015/FOTOS_RENDICION_DE_CUENTAS_2015.pdf" target="_blank">FOTOS RENDICIóN DE CUENTAS 2015.</a></u><br>
+      <u><a href="Archivos/rendicionCuentas2015/MARCO_ESTRATEGICO-_GADPR_BELLAMARIA.pdf" target="_blank">MARCO ESTRATéGICO- GADPR BELLAMARIA.</a></u><br>
+      <u><a href="Archivos/rendicionCuentas2015/PAPP_BELLAMARIA_2016.pdf" target="_blank">PAPP BELLAMARíA 2016.</a></u><br>
+
+      <u><a href="Archivos/rendicionCuentas2015/ACTA_DEL_CPCCS_Y_EL_CONCEJO_DE_PLANIFICACION.pdf" target="_blank">ACTA DEL CPCCS Y EL CONCEJO DE PLANIFICACION.</a></u><br>
+      <u><a href="Archivos/rendicionCuentas2015/PAPP_BELLAMARIA_2016.pdf" target="_blank">ACTA PROVISIONAL PUENTE PIEDRA REDONDA SEGUNDA FASE.</a></u><br>
+      <u><a href="Archivos/rendicionCuentas2015/ACTA_PROVISIONAL_PUENTE_PIEDRA_REDONDA_SEGUNDA_FASE.pdf" target="_blank">ACTA PROVISIONAL PUENTE PIEDRA REDONDA SEGUNDA FASE.</a></u><br>
+      <u><a href="Archivos/rendicionCuentas2015/ACTA_RECEPCION_DEFINITIVA_CUBIERTA_EL_RECREO.pdf" target="_blank">PAPP BELLAMARíA 2016.</a></u><br>
+      <u><a href="Archivos/rendicionCuentas2015/CATALOGO_ELECTRONICO_MAT_OFIC_GADPR.pdf" target="_blank">ACTA RECEPCION DEFINITIVA CUBIERTA EL RECREO.</a></u><br>
+      <u><a href="Archivos/rendicionCuentas2015/CATALOGO_ELECTRONICO_MAT_OFIC_GADPR.pdf" target="_blank">CATALOGO ELECTRONICO MAT OFIC GADPR.</a></u><br>
+      <u><a href="Archivos/rendicionCuentas2015/CATALOGO_ELECTRONICO_MAT_OFIC_PROYECTO_DISCAPACIDAD.pdf" target="_blank">CATALOGO ELECTRONICO MAT OFIC PROYECTO DISCAPACIDAD.</a></u><br>
+      <u><a href="Archivos/rendicionCuentas2015/CONTRATO_BASES_PUENTE_PARA_SITIO_BIRON.pdf" target="_blank">CONTRATO BASES PUENTE PARA SITIO BIRON.</a></u><br>
+      <u><a href="Archivos/rendicionCuentas2015/FOTOS_CONFORMACION_DEL_COMITÉ_DE_PARTICIPACION_CIUDADANA_Y_CONTROL_SOCIAL_DE_LA_PARROQUIA_BELLAMARIA.pdf" target="_blank">FOTOS CONFORMACION DEL COMITÉ DE PARTICIPACION CIUDADANA Y CONTROL SOCIAL PARROQUIA BELLAMARIA.</a></u><br>
+      <u><a href="Archivos/rendicionCuentas2015/FOTOS_CONFORMACION_DEL_CONSEJO_DE_PLANIFICACION_PARTICIPATIVA_DE_LA_PARROQUIA_BELLAMARIA.pdf" target="_blank">FOTOS CONFORMACION DEL CONSEJO DE PLANIFICACION PARTICIPATIVA DE LA PARROQUIA BELLAMARIA.</a></u><br>
+      <u><a href="Archivos/rendicionCuentas2015/FOTOS_CUBIERTA_METALICA_SITIO_EL_RECREO.pdf" target="_blank">FOTOS CUBIERTA METALICA SITIO EL RECREO.</a></u><br>
+      <u><a href="Archivos/rendicionCuentas2015/FOTOS_INICIO_CONSTRUCCION_BASES_PUENTE_PARA_SITIO_BIRON.pdf" target="_blank">FOTOS INICIO CONSTRUCCION BASES PUENTE PARA SITIO BIRON.</a></u><br>
+      <u><a href="Archivos/rendicionCuentas2015/FOTOS_OBRA_PUENTE_PIEDRA_REDONDA_SEGUNDA_FASE.pdf" target="_blank">FOTOS OBRA PUENTE PIEDRA REDONDA SEGUNDA FASE.</a></u>
+    
+    </p>
+  </div>
+</div>
+
+
+<?php include 'videos.php' ?>
