5805a33be0fc3e2277e799acf5cf097d2596c779
kerly1111 <krmc_94@hotmail.com>
2016-05-22 21:49:42 -0500

    final

--- galeria.php
+++ galeria.php
@@ -7,31 +7,18 @@
 <?php 
         require("clases/conexion.php");
         $con = conectar();
-        $sql = "SELECT * FROM modulo WHERE tip_modulo='GALERIA' ORDER BY id_modulo DESC";
-        $stmt = $con->prepare($sql);
-  
-        $result = $stmt->execute();
-        $rows = $stmt->fetchAll(\PDO::FETCH_OBJ);
-        foreach($rows as $row){
-
-        ?>
-        <h3><a class="stily-titulo" href="modulo.php?saludo=<?php print($row->id_modulo);?>"><?php print ($row->tit_modulo);?></a></h3>
-        <p ><?php print($row->fec_modulo);?></p>
-    	<p style="text-align: justify;" >
-         <?php print ($row->des_modulo);?>..... </p>
-
-        <?php
-          $sql2 = "SELECT * FROM imagenes WHERE modulo_id_modulo=".($row->id_modulo);
+        
+          $sql2 = "SELECT * FROM imagenes";
           $stmt2 = $con->prepare($sql2);
           $result2 = $stmt2->execute();
           $rows2 = $stmt2->fetchAll(\PDO::FETCH_OBJ);
           foreach($rows2 as $row2){
             $img=($row2->nom_imagen);
             ?>
-            <center><img src="imagenes/modulos/<?php print $img?>" alt="<?php print($row->id_modulo);?>" class="fotoModulo "></center>
+            <center><img src="imagenes/modulos/<?php print $img?>" alt="$img" class="fotoModulo "></center>
     <?php
           }
-          }          
+                    
 	?>
 
 
